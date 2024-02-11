8aae5ee165aaa0618239b5fe599af70f3d84a868
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-02-11 18:58:01 +0100

    Added methods Exports

--- src/BiGGQuery.jl
+++ src/BiGGQuery.jl
@@ -7,10 +7,23 @@ using URIParser
 using Logging
 
 include("utils.jl")
-include("search.jl")
+export api_request, get_bigg_database_version
+
 include("universal.jl")
+export get_bigg_universal_metabolite_details, get_bigg_universal_metabolites, 
+        get_bigg_universal_reaction_details, get_bigg_universal_reactions
+
 include("models.jl")
+export get_bigg_model_details, get_bigg_model_genes, get_bigg_model_metabolite_details, 
+        get_bigg_model_metabolites, get_bigg_model_reaction_details, get_bigg_model_reactions, 
+        get_bigg_models, get_bigg_model_gene_details
+
 include("downloads.jl")
+export download_bigg_model, download_bigg_model_file
+
+include("search.jl")
+export search_bigg_models
+
 
 # Configure logging
 logger = SimpleLogger(stdout, Logging.Info)
