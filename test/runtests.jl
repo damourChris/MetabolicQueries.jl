b61ac52f2fe000bcc02ba70f462b345a657b374f
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-02-12 00:46:10 +0100

    Updated and reintroduce downloads test in test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using BiGGQuery, Test
 @testset "BiGGQuery Search Tests" begin
     include("./test_search.jl")
     include("./test_universal.jl")
-    # include("./test_downloads.jl")
+    include("./test_downloads.jl")
     include("./test_models.jl")
     include("./test_utils.jl")
 end
--- test/test_downloads.jl
+++ test/test_downloads.jl
@@ -4,28 +4,21 @@ function test_download_bigg_model()
     model_id = "iND750"
     response = download_bigg_model(model_id)
     @test typeof(response) == Dict{String, Any}
-
-    # Test case 2: Downloading a non-existent model
-    model_id = "nonexistent_model"
-    response = download_bigg_model(model_id)
-    @test response == nothing
 end
 
 # Test download_bigg_model_file function
 function test_download_bigg_model_file()
     # Test case 1: Downloading a valid model file
     model_id = "iND750"
-    file_type = "xml.gz"
+    file_type = "xml"
     file_path = "."
     downloaded_file = download_bigg_model_file(model_id, file_type, file_path)
+    @test downloaded_file[findlast(==('.'), downloaded_file)+1:end] == file_type
     @test typeof(downloaded_file) == String
     @test isfile(downloaded_file)
+    rm(downloaded_file)
 
-    # Test case 2: Downloading a model file with invalid file type
-    model_id = "iND750"
-    file_type = "txt"
-    file_path = "."
-    @test_throws ArgumentError download_bigg_model_file(model_id, file_type, file_path)
+
 end
 
 # Run tests
