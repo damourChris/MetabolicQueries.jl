b21efc9931b2b733e068c670c06fb2110c8bd340
Christopher Damour <129872252+damourChris@users.noreply.github.com>
2024-02-16 01:33:16 +0100

    [TEST] Cleanup old tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,14 @@
-using BiGGQuery, Test
+using MetabolicQueries, Test
 
-@testset "BiGGQuery Search Tests" begin
-    include("./test_search.jl")
-    include("./test_universal.jl")
-    include("./test_downloads.jl")
-    include("./test_models.jl")
-    include("./test_utils.jl")
+@testset "Mock API Tests" begin
+    
+end
+@testset "BiGGQuery Tests" begin
+    
+end
+@testset "KEGG Tests" begin
+    
+end
+@testset "MetabolicAtlas Tests" begin
+    
 end
